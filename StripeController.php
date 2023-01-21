c43e4a47fa026093a448117dca196c7cf56de046
michael <m@cadot.eu>
2023-01-21 16:48:27 +0100

    add stripe

---
+++ StripeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\base;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Stripe;
+
+class StripeController extends AbstractController
+{
+    #[Route('/stripe', name: 'app_stripe')]
+    public function index(): Response
+    {
+        return $this->render('/base/stripe.html.twig',);
+    }
+
+
+    #[Route('/stripe/create-charge', name: 'app_stripe_charge', methods: ['POST'])]
+    public function createCharge(Request $request)
+    {
+        Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+        Stripe\Charge::create([
+            "amount" => 5 * 100,
+            "currency" => "usd",
+            "source" => $request->request->get('stripeToken'),
+            "description" => "Binaryboxtuts Payment Test"
+        ]);
+        $this->addFlash(
+            'success',
+            'Payment Successful!'
+        );
+        return $this->redirectToRoute('app_stripe', [], Response::HTTP_SEE_OTHER);
+    }
+}
